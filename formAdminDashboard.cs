b718cdb4009d1495deb6d66984850224ae5460c0
Patil-Lingaraj <90733597+Patil-Lingaraj@users.noreply.github.com>
2021-10-05 19:30:20 -0400

    Add files via upload

---
+++ formAdminDashboard.cs
@@ -0,0 +1,42 @@
+﻿using Bookstore___Invoice_System.User_Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bookstore___Invoice_System
+{
+    public partial class formAdminDashboard : Form
+    {
+        public formAdminDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void formAdminDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelSubHead_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelLoggedInEmployee_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void employeesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            formEmployees employee = new formEmployees();
+            employee.Show();
+        }
+    }
+}
