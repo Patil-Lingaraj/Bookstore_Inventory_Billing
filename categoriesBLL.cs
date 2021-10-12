509a81b33c770c8a77a1aa5ff9c213b000e1c9ab
Shubham Daule <90416584+ShubhamDaule@users.noreply.github.com>
2021-10-12 01:55:42 -0400

    Add files via upload
    Added the functionality code for ADD, UPDATE, DELETE and SEARCH key.

--- categoriesBLL.cs
+++ categoriesBLL.cs
@@ -12,7 +12,7 @@ namespace BookStore.BLL
         public string Book Genre ID { get; set; }
         public string Book Genre Description { get; set; }
         
-        public DateTime added_Date { get; set; }
+        public DateTime Added_Date { get; set; }
     
         public int Added by Employee_ID { get; set; }
 }
