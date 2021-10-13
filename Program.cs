f01367393d8092003d46ae71f57f76f3ca5dcd52
Patil-Lingaraj <90733597+Patil-Lingaraj@users.noreply.github.com>
2021-10-12 20:12:59 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bookstore___Invoice_System.User_Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Bookstore___Invoice_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new formAdminDashboard());
+            Application.Run(new formLogin());
         }
     }
 }
